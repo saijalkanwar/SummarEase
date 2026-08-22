bae399ba792b4b04326206b1260a6666c0992a51
Saijal Kanwar <saijalkanwar71@gmail.com>
2026-08-22 22:37:22 +0530

    fixed bugs

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Document Summary Assistant API")
     
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://summar-ease-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
